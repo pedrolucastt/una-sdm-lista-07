bc02b87c6ffbbf75a7db5b647e2d79ba27e1d738
Pedro Lucas Dias Amorim de Deus <pedrodiastt7@gmail.com>
2026-03-22 12:06:28 -0300

    Corrigindo README

--- README.md
+++ README.md
@@ -4,7 +4,27 @@ API desenvolvida em .NET para fornecer dicas de sustentabilidade.
 
 ## 🚀 Funcionalidades
 
-- Retorna uma dica de sustentabilidade aleatória
-- Endpoint simples utilizando método GET
+- Retorna uma dica de sustentabilidade aleatória  
+- Endpoint simples utilizando método GET  
 
 ## 🔗 Endpoint
+
+GET /Sustentabilidade
+
+## 💡 Exemplo de resposta
+
+"Evite desperdício de água."
+
+## 🛠️ Tecnologias utilizadas
+
+- C#  
+- .NET  
+- ASP.NET Core Web API  
+
+📸 Teste da API
+![alt text](image.png)
+
+## ▶️ Como executar
+
+```bash
+dotnet run
--- obj/Debug/net10.0/EcoTrackApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/EcoTrackApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoTrackApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e16257e0ace16149a895fce1d780cfe6df70959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0bf087c2d4e535bf612ce616616d99ba59df206")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoTrackApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoTrackApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
